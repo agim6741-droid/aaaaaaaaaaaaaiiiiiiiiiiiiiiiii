a6af10ddbba0db1e892361e2ff9197e4a652c125
agim6741-droid <agim6741@gmail.com>
2025-11-23 22:30:56 +0900

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-st.title('나의 첫 웹 서비스 만들기')
-st.write('안녕하세요, 만나서 반갑습니다.')
-name=st.text_input('이름을 입력해주세요.')
-if st.button('인삿말 생성'):
-  st.write(name+'님 반갑습니다.')
-  st.balloons()
